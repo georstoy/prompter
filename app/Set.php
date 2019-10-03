db6cbd6e3065deb5a7e5773a5ecbb5de65fb3a8e
georstoy <georstoy@gmail.com>
2019-10-03 18:37:01 +0300

    Set extends PromptableModel

--- app/Set.php
+++ app/Set.php
@@ -2,24 +2,8 @@
 
 namespace App;
 
-use App\Traits\Fetcher;
-use Jenssegers\Mongodb\Eloquent\Model as MongoModel;
-
-class Set extends MongoModel
+class Set extends PromptableModel
 {
-    use Fetcher;
-
     protected $collection = 'sets';
 
-    protected $fillable = [
-        'request'
-    ];
-
-    protected $guarded = [
-        '_id'
-    ];
-
-    protected $primaryKey = '_id';
-
-    public $incrementing = true;
 }
